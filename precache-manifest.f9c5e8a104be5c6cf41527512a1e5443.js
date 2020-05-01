89307411742cf45963013c47815aa8b4a227d45d
Sergey Belov <48603698+Lowelys@users.noreply.github.com>
2020-05-01 17:39:34 +0300

    Updates

---
+++ precache-manifest.f9c5e8a104be5c6cf41527512a1e5443.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a220d706b03ad6c1db6951d4dbeefd35",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b0dd92590de7edcd2536",
+    "url": "/react-portfolio/static/css/main.07c08b02.chunk.css"
+  },
+  {
+    "revision": "5ac3add2fbef3e4c1352",
+    "url": "/react-portfolio/static/js/2.01b38c46.chunk.js"
+  },
+  {
+    "revision": "b0dd92590de7edcd2536",
+    "url": "/react-portfolio/static/js/main.349c3af9.chunk.js"
+  },
+  {
+    "revision": "08f5cadb00c2d4c9874d",
+    "url": "/react-portfolio/static/js/runtime~main.42537fbc.js"
+  },
+  {
+    "revision": "471068b62dcb910e45d66ec084f5a6f4",
+    "url": "/react-portfolio/static/media/bootstrap-icon.471068b6.png"
+  },
+  {
+    "revision": "cfe38c5b2a6bbdc055144b6311e9d402",
+    "url": "/react-portfolio/static/media/counter.cfe38c5b.jpg"
+  },
+  {
+    "revision": "d99ab59a0de35d355c1ded657c5c6cdc",
+    "url": "/react-portfolio/static/media/css-icon.d99ab59a.png"
+  },
+  {
+    "revision": "325d6e7179b87b1711949cf945539bc1",
+    "url": "/react-portfolio/static/media/git-icon.325d6e71.png"
+  },
+  {
+    "revision": "7da7b7c8a6d896c0ddfc8475d2e351f3",
+    "url": "/react-portfolio/static/media/github.7da7b7c8.png"
+  },
+  {
+    "revision": "397b994bcc026515bd5fbf5f44a1398a",
+    "url": "/react-portfolio/static/media/gmail.397b994b.png"
+  },
+  {
+    "revision": "148ff8bd87c7b7535c20b42259786317",
+    "url": "/react-portfolio/static/media/html-icon.148ff8bd.png"
+  },
+  {
+    "revision": "5a4a2142071fb9b32e5c0cab0894b657",
+    "url": "/react-portfolio/static/media/js-icon.5a4a2142.png"
+  },
+  {
+    "revision": "a59986274efb5d301efcd1b2c8bbc91d",
+    "url": "/react-portfolio/static/media/linkedin.a5998627.png"
+  },
+  {
+    "revision": "d4c8b48eef722b92fde136f114a68ef2",
+    "url": "/react-portfolio/static/media/nodejs.d4c8b48e.png"
+  },
+  {
+    "revision": "0273897ee1b5d2bd97b273ebb95595a5",
+    "url": "/react-portfolio/static/media/photo1.0273897e.png"
+  },
+  {
+    "revision": "c3b9b403fb9b28d75e39e56e1861eec1",
+    "url": "/react-portfolio/static/media/portfolio.c3b9b403.jpg"
+  },
+  {
+    "revision": "83d6dc0f2b65e1ff0074ce1cdfe40b8c",
+    "url": "/react-portfolio/static/media/react-icon.83d6dc0f.png"
+  },
+  {
+    "revision": "f1da3603cc45da850de9651f7bbe5fe0",
+    "url": "/react-portfolio/static/media/redux-icon.f1da3603.png"
+  },
+  {
+    "revision": "7dad14cb6449042a04e0b8fb1a95006b",
+    "url": "/react-portfolio/static/media/sass-icon2.7dad14cb.png"
+  },
+  {
+    "revision": "b0d52922afc311d51ff9c3c299b22d27",
+    "url": "/react-portfolio/static/media/social-network.b0d52922.jpg"
+  },
+  {
+    "revision": "0fc6bb252949707c33074cf9751bc45f",
+    "url": "/react-portfolio/static/media/telegram.0fc6bb25.png"
+  },
+  {
+    "revision": "1d5313338b6eb049540506d91a781a5d",
+    "url": "/react-portfolio/static/media/temp-icon.1d531333.png"
+  },
+  {
+    "revision": "afff4dffcce86485022a5d90a396a5c9",
+    "url": "/react-portfolio/static/media/to-top.afff4dff.png"
+  },
+  {
+    "revision": "1454ea4b1f7291aefc8878e1dfdafb52",
+    "url": "/react-portfolio/static/media/tournament-table.1454ea4b.jpg"
+  },
+  {
+    "revision": "beb2a760c817b3e83b6a3e33fae01cbd",
+    "url": "/react-portfolio/static/media/vk.beb2a760.png"
+  }
+]);
